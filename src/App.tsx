868179a71ded399722a1ed4876dd0d328d881582
Abdessadek <abdessadekhm@gmail.com>
2026-03-02 14:01:59 +0000

    [ADD] Initialize the project

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { Button, Flex, Text } from "@radix-ui/themes"
+
+function App() {
+
+  return (
+    <>
+    <Flex direction="row" gap="3" justify="center" align="center" m="2" width="full" height={'100vh'}>
+      <Text>Test Radix UI :</Text>
+      <Button>Let's go</Button>
+    </Flex>
+      
+    </>
+  )
+}
+
+export default App
